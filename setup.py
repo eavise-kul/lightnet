77cf5ad78bae8fc2d661a77578ff6d386c185e96
0phoff <tanguy.ophoff@kuleuven.be>
2017-11-08 14:17:21 +0100

    Created library structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lightnet',
+      version='0.0.1',
+      description='Building blocks for recreating darknet networks in pytorch',
+      author='EAVISE',
+      packages=['lightnet',
+      scripts=[],
+)
