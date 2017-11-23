086cfa1c44ed9e8f9237a2aad0ed33ccf1454d2a
0phoff <tanguy.ophoff@kuleuven.be>
2017-11-23 17:33:38 +0100

    Added brambox and darknet datasets

--- lightnet/__init__.py
+++ lightnet/__init__.py
@@ -7,3 +7,4 @@ from .weights import *
 from .network import *
 from .bbox import *
 from .loss import *
+from .dataset import *
---
+++ lightnet/dataset.py
@@ -0,0 +1,100 @@
+#
+#   Darknet specific dataset
+#   Copyright EAVISE
+#   
+
+import os
+import torch
+from torch.utils.data.dataset import Dataset
+from torchvision import transforms as tf
+from PIL import Image
+import brambox.boxes as bbb
+
+from .transforms import *
+from .logger import *
+
+
+class BramboxData(Dataset):
+    """ Dataset for any brambox parsable annotation format
+        
+        anno_format:    annotation format from brambox.boxes.format
+        anno_filename:  annotation filename, list of filenames or expandable sequence
+        identify:       lambda/function to get image based of annotation filename or image id
+                        DEFAULT: replace/add .png extension to filename/id
+        img_transform:  transforms to perform on the images
+        anno_transform: transforms to perform on the annotations
+        kwargs:         keyword arguments that are passed to the brambox parser
+    """
+    def __init__(self, anno_format, anno_filename, identify=None, img_transform=None, anno_transform=None, **kwargs):
+        super(BramboxData, self).__init__()
+        self.img_tf = img_transform
+        self.anno_tf = anno_transform
+        if identify is not None and callable(identify):
+            self.id = identify
+        else:
+            self.id = lambda name : os.path.splitext(name)[0] + '.png'
+
+        # Get annotations
+        self.annos = bbb.parse(anno_format, anno_filename, identify=lambda f:f, **kwargs)
+        self.keys = list(self.annos)
+        log(Loglvl.VERBOSE, f'Dataset loaded: {len(self.keys)} images')
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, index):
+        """ Get (img, anno) tuple based of index from self.keys """
+        # Load
+        img = Image.open(self.id(self.keys[index]))
+        anno = self.annos[self.keys[index]]
+
+        # Transform
+        if self.img_tf is not None:
+            img = self.img_tf(img)
+        if self.anno_tf is not None:
+            anno = self.anno_tf(anno)
+
+        # Return
+        return img, anno
+
+
+class DarknetData(BramboxData):
+    """ Dataset that works with darknet files and performs the same data augmentations
+        
+        data_file:  file containing path to annotation files (relative from where command is run)
+        network:    network that will be used with dataset (needed for network input dimension)
+        max_anno:   Maximum number of annotations per image
+        augment:    Boolean indicating whether you want data augmentation
+        jitter:     Determines random crop sizes
+        flip:       Determines whether image will be flipped
+        hue:        Determines hue shift
+        saturation: Determines saturation shift
+        value:      Determines value (exposure) shift
+    """
+    def __init__(self, data_file, network, augment=True, jitter=.2, flip=.5, hue=.1, saturation=1.5, value=1.5):
+        with open(data_file, 'r') as f:
+            self.img_paths = f.read().splitlines()
+
+        # Prepare variables for brambox init
+        anno_format = 'anno_darknet'
+        self.anno_paths = [os.path.splitext(p)[0]+'.txt' for p in self.img_paths]
+        identify = lambda name : self.img_paths[self.anno_paths.index(name)]
+        
+        lb  = Letterbox(network)
+        rf  = RandomFlip(flip)
+        rc  = RandomCrop(jitter, True)
+        hsv = HSVShift(hue, saturation, value)
+        at  = AnnoToTensor(network)
+        it  = tf.ToTensor()
+        if augment:
+            img_tf = tf.Compose([hsv, rc, rf, lb, it])
+            anno_tf = tf.Compose([rc, rf, lb, at])
+        else:
+            img_tf = tf.Compose([lb, it])
+            anno_tf = tf.Compose([lb, at])
+        
+        first_img = Image.open(self.img_paths[0])
+        w, h = first_img.size
+        kwargs = { 'image_width': w, 'image_height':h, 'class_label_map': None }
+
+        super(DarknetData, self).__init__(anno_format, self.anno_paths, identify, img_tf, anno_tf, **kwargs)
