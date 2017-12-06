a342ee9cfd91b005e241fa120c62e84c1fd9d8e0
0phoff <tanguy.ophoff@kuleuven.be>
2017-12-06 15:07:05 +0100

    Reworked bbox
    Added _get_region_box_nms which performs region box and nms in one loop (must test if faster)
    
    Added bbox_multi_ious
    
    Reformatted comments

--- lightnet/bbox.py
+++ lightnet/bbox.py
@@ -10,7 +10,7 @@ from brambox.boxes.detections.detection import *
 
 from .logger import *
 
-__all__ = ['BBoxConverter', 'BBoxToBrambox']
+__all__ = ['BBoxConverter', 'bbox_to_brambox']
 
 
 class BBoxConverter:
@@ -23,16 +23,19 @@ class BBoxConverter:
     def __init__(self, network, conf_thresh, nms_thresh):
         self.conf_thresh = conf_thresh
         self.nms_thresh = nms_thresh
-        self.anchors = network.anchors
         self.num_classes = network.num_classes
+        self.anchors = network.anchors
         self.num_anchors = network.num_anchors
+        self.anchor_step = len(self.anchors) // self.num_anchors
 
     def __call__(self, network_output):
         """ Compute bounding boxes after thresholding and nms
             
             network_output  Output tensor from the lightnet network
         """
-        boxes = self._get_region_boxes_nms(network_output.data)
+        #boxes = self._get_region_boxes_nms(network_output.data)
+        all_boxes = self._get_region_boxes(network_output.data)
+        boxes = [self._nms(box) for box in all_boxes]
         return boxes
 
     def _get_region_boxes_nms(self, output):
@@ -41,17 +44,17 @@ class BBoxConverter:
         if output.dim() == 3:
             output.unsqueeze_(0)
 
-        # Variables
+        # Parameters
         cuda = output.is_cuda
         batch = output.size(0)
         h = output.size(2)
         w = output.size(3)
 
-        # Compute xc,yc, w,h, box_score on Tensor
+        # Compute xc,yc, w,h, box_score
         lin_x = torch.linspace(0, w-1, w).repeat(h,1).view(h*w)
         lin_y = torch.linspace(0, h-1, h).repeat(w,1).t().contiguous().view(h*w)
-        anchor_w = torch.Tensor(self.anchors[::2]).view(1, self.num_anchors, 1)
-        anchor_h = torch.Tensor(self.anchors[1::2]).view(1, self.num_anchors, 1)
+        anchor_w = torch.Tensor(self.anchors[::self.anchor_step]).view(self.num_anchors, 1)
+        anchor_h = torch.Tensor(self.anchors[1::self.anchor_step]).view(self.num_anchors, 1)
         if cuda:
             lin_x = lin_x.cuda()
             lin_y = lin_y.cuda()
@@ -86,41 +89,122 @@ class BBoxConverter:
         for b in range(batch):
             box_batch = []
             for i in range(self.num_anchors*h*w):
-                    idx = cls_max_sort_id[b,i]
-                    if cls_max[b,idx] < 0:
-                        continue
-                    if cls_max[b,idx] < self.conf_thresh:
+                idx = cls_max_sort_id[b,i]
+                if cls_max[b,idx] < 0:
+                    continue
+                if cls_max[b,idx] < self.conf_thresh:
+                    break
+
+                a = idx // (h*w)
+                hw = idx % (h*w)
+                box_batch.append([
+                    output[b,a,0,hw],
+                    output[b,a,1,hw],
+                    output[b,a,2,hw],
+                    output[b,a,3,hw],
+                    cls_max[b,idx],
+                    cls_max_idx[b,idx]
+                ])
+
+                for j in range(i+1, self.num_anchors*h*w):
+                    idx2 = cls_max_sort_id[b,j]
+                    a2 = idx2 // (h*w)
+                    hw2 = idx2 % (h*w)
+                    if cls_max[b,idx2] >= 0 and cls_max[b,idx2] < self.conf_thresh:
                         break
-
-                    a = idx // (h*w)
-                    hw = idx % (h*w)
-                    box_batch.append([
-                        output[b,a,0,hw],
-                        output[b,a,1,hw],
-                        output[b,a,2,hw],
-                        output[b,a,3,hw],
-                        cls_max[b,idx],
-                        cls_max_idx[b,idx]
-                    ])
-
-                    for j in range(i+1, self.num_anchors*h*w):
-                        idx2 = cls_max_sort_id[b,j]
-                        a2 = idx2 // (h*w)
-                        hw2 = idx2 % (h*w)
-                        if cls_max[b,idx2] >= 0 and cls_max[b,idx2] < self.conf_thresh:
-                            break
-                        if cls_max[b,idx2] >= 0 and bbox_iou(box_batch[-1], output[b,a2,0:4,hw2]) > self.nms_thresh:
-                            cls_max[b,idx2] = -1
-                            
+                    if cls_max[b,idx2] >= 0 and bbox_iou(box_batch[-1], output[b,a2,0:4,hw2]) > self.nms_thresh:
+                        cls_max[b,idx2] = -1
+                        
             boxes.append(box_batch)
 
         return boxes
 
+    def _get_region_boxes(self, output):
+        """ Returns array of detections for every image in batch """
+        # Check dimensions
+        if output.dim() == 3:
+            output.unsqueeze_(0)
 
-def BBoxToBrambox(boxes, net_size, img_size=None, class_label_map=None):
+        # Variables
+        cuda = output.is_cuda
+        batch = output.size(0)
+        h = output.size(2)
+        w = output.size(3)
+
+        # Compute xc,yc, w,h, box_score on Tensor
+        lin_x = torch.linspace(0, w-1, w).repeat(h,1).view(h*w)
+        lin_y = torch.linspace(0, h-1, h).repeat(w,1).t().contiguous().view(h*w)
+        anchor_w = torch.Tensor(self.anchors[::2]).view(1, self.num_anchors, 1)
+        anchor_h = torch.Tensor(self.anchors[1::2]).view(1, self.num_anchors, 1)
+        if cuda:
+            lin_x = lin_x.cuda()
+            lin_y = lin_y.cuda()
+            anchor_w = anchor_w.cuda()
+            anchor_h = anchor_h.cuda()
+
+        output = output.view(batch, self.num_anchors, -1, h*w)  # -1 == 5+num_classes (we can drop feature maps if 1 class)
+        output[:,:,0,:].sigmoid_().add_(lin_x).div_(w)          # X center
+        output[:,:,1,:].sigmoid_().add_(lin_y).div_(h)          # Y center
+        output[:,:,2,:].exp_().mul_(anchor_w).div_(w)           # Width
+        output[:,:,3,:].exp_().mul_(anchor_h).div_(h)           # Height
+        output[:,:,4,:].sigmoid_()                              # Box score
+
+        # Compute class_score
+        if self.num_classes > 1:
+            cls_scores = torch.nn.functional.softmax(Variable(output[:,:,5:,:], volatile=True), 2).data
+            cls_max, cls_max_idx = torch.max(cls_scores, 2)
+            cls_max.mul_(output[:,:,4,:])
+        else:
+            cls_max = output[:,:,4,:]
+            cls_max_idx = torch.zeros_like(cls_max)
+
+        # Save detection if conf*class_conf is higher than threshold
+        output = output.cpu()
+        cls_max = cls_max.cpu()
+        boxes = []
+        for b in range(batch):
+            box_batch = []
+            for a in range(self.num_anchors):
+                for i in range(h*w):
+                    if cls_max[b,a,i] > self.conf_thresh:
+                        box_batch.append([
+                            output[b,a,0,i],
+                            output[b,a,1,i],
+                            output[b,a,2,i],
+                            output[b,a,3,i],
+                            cls_max[b,a,i],
+                            cls_max_idx[b,a,i]
+                            ])
+            boxes.append(box_batch)
+
+        return boxes
+
+    def _nms(self, detections):
+        """ Returns pruned detections after nms """
+        det_len = len(detections)
+
+        if len(detections) == 0:
+            return detections
+
+        det_scores = torch.Tensor([det[4] for det in detections])
+        _,sortIds = torch.sort(det_scores, 0, True)
+        out_detections = []
+        for i in range(det_len):
+            det_i = detections[sortIds[i]]
+            if det_i[4] > 0:
+                out_detections.append(det_i)
+                for j in range(i+1, det_len):
+                    det_j = detections[sortIds[j]]
+                    if det_j[4] > 0 and bbox_iou(det_i, det_j) > self.nms_thresh:
+                        det_j[4] = 0
+
+        return out_detections
+
+
+def bbox_to_brambox(boxes, net_size, img_size=None, class_label_map=None):
     """ Convert bounding box array to brambox detection object
         
-        boxes               Array of detection boxes (eg. output of 1 batch of a network)
+        boxes               Array of detection boxes
         net_size            (width, height) sequence of the input size of the network
         [img_size]          (width, height) sequence of the final image
         [class_label_map]   array of class labels
@@ -150,9 +234,9 @@ def BBoxToBrambox(boxes, net_size, img_size=None, class_label_map=None):
         det.height = box[3] * net_h
         det.confidence = box[4]*100
         if class_label_map is not None:
-            det.class_label = class_label_map[box[5]]
+            det.class_label = class_label_map[int(box[5])]
         else:
-            det.class_label = int(box[5])
+            det.class_label = str(int(box[5]))
 
         det.x_top_left -= pad[0]
         det.y_top_left -= pad[1]
@@ -167,10 +251,10 @@ def bbox_iou(box1, box2):
     """ Compute IOU between 2 bounding boxes
         Box format: [xc, yc, w, h]
     """
-    mx = min(box1[0]-box1[2]/2.0, box2[0]-box2[2]/2.0)
-    Mx = max(box1[0]+box1[2]/2.0, box2[0]+box2[2]/2.0)
-    my = min(box1[1]-box1[3]/2.0, box2[1]-box2[3]/2.0)
-    My = max(box1[1]+box1[3]/2.0, box2[1]+box2[3]/2.0)
+    mx = min(box1[0]-box1[2]/2, box2[0]-box2[2]/2)
+    Mx = max(box1[0]+box1[2]/2, box2[0]+box2[2]/2)
+    my = min(box1[1]-box1[3]/2, box2[1]-box2[3]/2)
+    My = max(box1[1]+box1[3]/2, box2[1]+box2[3]/2)
     w1 = box1[2]
     h1 = box1[3]
     w2 = box2[2]
@@ -181,10 +265,36 @@ def bbox_iou(box1, box2):
     iw = w1 + w2 - uw
     ih = h1 + h2 - uh
     if iw <= 0 or ih <= 0:
-        return 0.0
+        return 0
 
     area1 = w1 * h1
     area2 = w2 * h2
     iarea = iw * ih
+    uarea = area1 + area2 - iarea
+    return iarea/uarea
+
+
+def bbox_multi_ious(boxes1, boxes2):
+    """ Compute IOU between 2 lists of bounding boxes
+        List format: [[xc, yc, w, h],...]
+    """
+    mx = torch.min(boxes1[:,0]-boxes1[:,2]/2, boxes2[:,0]-boxes2[:,2]/2)
+    Mx = torch.max(boxes1[:,0]+boxes1[:,2]/2, boxes2[:,0]+boxes2[:,2]/2)
+    my = torch.min(boxes1[:,1]-boxes1[:,3]/2, boxes2[:,1]-boxes2[:,3]/2)
+    My = torch.max(boxes1[:,1]+boxes1[:,3]/2, boxes2[:,1]+boxes2[:,3]/2)
+    w1 = boxes1[:,2]
+    h1 = boxes1[:,3]
+    w2 = boxes2[:,2]
+    h2 = boxes2[:,3]
+
+    uw = Mx - mx
+    uh = My - my
+    iw = w1 + w2 - uw
+    ih = h1 + h2 - uh
+
+    area1 = w1 * h1
+    area2 = w2 * h2
+    iarea = iw * ih
+    iarea[(iw <= 0) + (ih <= 0) > 0] = 0
     uarea = area1 + area2 - iarea
     return iarea/uarea
