67a061831e8b883c94a829cf8c82ec12f28e8ac5
0phoff <tanguy.ophoff@kuleuven.be>
2017-12-13 14:11:24 +0100

    Updated examples to new structure

--- examples/detect.py
+++ examples/detect.py
@@ -15,8 +15,6 @@ from torch.autograd import Variable
 import brambox.boxes as bbb
 
 import lightnet as ln
-import lightnet.transforms as lnt
-from lightnet.models import *
 ln.log.level = ln.Loglvl.VERBOSE
 
 # Parameters
@@ -30,7 +28,7 @@ TIMES_NETWORK = []
 # Functions
 def create_network():
     """ Create the lightnet network """
-    net = YoloVoc(CLASSES, args.weight, NETWORK_SIZE)
+    net = ln.models.YoloVoc(CLASSES, args.weight, NETWORK_SIZE)
 
     if args.cuda:
         net.cuda()
@@ -43,7 +41,7 @@ def detect(net, img_path):
     # Load image
     img = cv2.imread(img_path)
     preprocess = transforms.Compose([
-            lnt.Letterbox(net),
+            ln.data.Letterbox(net),
             transforms.ToTensor()
         ])
     img_tensor = preprocess(img)
@@ -65,18 +63,19 @@ def detect(net, img_path):
 
 def draw_boxes(img, det, net_dim):
     """ Draw detections and annotations on the image """
-    det = ln.bbox_to_brambox(det, net_dim, (img.shape[1], img.shape[0]), names)
-    bbb.draw_box(img, det, show_labels=True, inline=True)
+    det = ln.data.bbox_to_brambox(det, net_dim, (img.shape[1], img.shape[0]), names)
+    bbb.draw_box(img, det, show_labels=args.label, inline=True)
     return img
 
 # Main
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run a single image through a lightnet network')
     parser.add_argument('weight', help='Path to weight file')
-    parser.add_argument('image', help='Path to image file', nargs='?')
+    parser.add_argument('image', help='Path to image file', nargs='*')
     parser.add_argument('-n', '--names', help='path to names file', default=None)
-    parser.add_argument('-s', '--save', action='store_true', help='Save image in stead of displaying it')
     parser.add_argument('-c', '--cuda', action='store_true', help='Use cuda')
+    parser.add_argument('-s', '--save', action='store_true', help='Save image in stead of displaying it')
+    parser.add_argument('-l', '--label', action='store_true', help='Print labels and scores on the image')
     args = parser.parse_args()
 
     # Parse Arguments
@@ -94,15 +93,17 @@ if __name__ == '__main__':
     network = create_network()
 
     # Detection
-    if args.image is not None:
-        image, output = detect(network, args.image)
-        image = draw_boxes(image, output[0], network.input_dim[:2])
-        if args.save:
-            cv2.imwrite('detections.png', image)
-        else:
-            cv2.imshow('image', image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+    if len(args.image) > 0:
+        for img_name in args.image:
+            print(img_name)
+            image, output = detect(network, img_name)
+            image = draw_boxes(image, output[0], network.input_dim[:2])
+            if args.save:
+                cv2.imwrite('detections.png', image)
+            else:
+                cv2.imshow('image', image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
     else:
         while True:
             try:
@@ -117,9 +118,12 @@ if __name__ == '__main__':
 
             image, output = detect(network, img_path)
             image = draw_boxes(image, output[0], network.input_dim[:2])
-            cv2.imshow('image', image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            if args.save:
+                cv2.imwrite('detections.png', image)
+            else:
+                cv2.imshow('image', image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
 
     # Timer
     if TIMER:
--- examples/train.py
+++ examples/train.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 #
-#   Train a lightnet network
+#   Train a lightnet network with the engine
 #   Copyright EAVISE
 #
 
-from torchvision import transforms as tf
-
 import os
-import sys
 import argparse
 from statistics import mean
-import numpy as np
 import torch
 from torch.autograd import Variable
 import brambox.boxes as bbb
 
 import lightnet as ln
-import lightnet.transforms as lnt
-from lightnet.models import *
 ln.log.level = ln.Loglvl.VERBOSE
 #ln.log.color = False
 
 # Parameters
+WORKERS = 4
+PIN_MEM = True
+VISDOM = {'server': 'http://localhost', 'port': 8080, 'env': 'Lightnet'}
+
 CLASSES = 1
 NETWORK_SIZE = [416, 416, 3]
+RESIZE_RATE = 10
+CONF_THRESH = 0.1
+NMS_THRESH = 0.4
 
 BATCH = 64 
 BATCH_SUBDIV = 8
@@ -33,7 +34,7 @@ LEARNING_RATE = 0.0001              # Initial learning rate
 MOMENTUM = 0.9
 DECAY = 0.0005
 LR_STEPS = (100, 25000, 35000)      # Steps at which the learning rate should be scaled
-LR_SCALES = (10, 0.1, 0.1)          # Scales to scale the inital learning rate with
+LR_SCALES = (2, 0.5, 0.1)           # Scales to scale the inital learning rate with
 
 BACKUP = 100                        # Initial backup rate 
 BP_STEPS = (500, 5000, 10000)       # Steps at which the backup rate should change
@@ -43,234 +44,179 @@ TEST = 25                           # Initial test rate (only tested in between 
 TS_STEPS = (1000, 5000)             # Steps at which the test rate should change
 TS_RATES = (50, 100)                # New values for test rate
 
-
-# Parameter checks
 assert BATCH % BATCH_SUBDIV == 0, 'Batch subdivision should be a divisor of batch size'
 assert len(LR_STEPS) == len(LR_SCALES), 'Learning rate scales should have same number of items as steps'
 assert len(BP_STEPS) == len(BP_RATES), 'Backup rates should have same number of items as steps'
 
 
-def create_network():
-    """ Create the lightnet network and optimizer """
-    net = YoloVoc(CLASSES)
-    net.input_dim = NETWORK_SIZE
-    net.postprocess.conf_thresh = .1
-    net.load_weights(args.weight)
-    if args.cuda:
-        net.cuda()
+class CustomEngine(ln.engine.Engine):
+    """ This is a custom engine for this training cycle """
+    def __init__(self, arguments, **kwargs):
+        ln.log(ln.Loglvl.DEBUG, 'Creating network')
+        net = ln.models.YoloVoc(CLASSES, arguments.weight, NETWORK_SIZE, CONF_THRESH, NMS_THRESH)
+        if arguments.cuda:
+            net.cuda()
+        optim = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE/BATCH, momentum=MOMENTUM, dampening=0, weight_decay=DECAY*BATCH)
 
-    optim = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE/BATCH, momentum=MOMENTUM, dampening=0, weight_decay=DECAY*BATCH)
-
-    return net, optim
-
-def adjust_learning_rate(optim, batch):
-    """ Adjust the learning rate according to the batch number """
-    lr = LEARNING_RATE
-    for i in range(len(LR_STEPS)):
-        if batch >= LR_STEPS[i]:
-            lr *= LR_SCALES[i]
+        ln.log(ln.Loglvl.DEBUG, 'Creating datasets')
+        train = ln.models.DarknetData(arguments.train, net)
+        if arguments.test is not None:
+            test = ln.models.DarknetData(arguments.test, net, train=False, augment=False, class_label_map=arguments.names)
         else:
-            break
+            test = None
 
-    ln.log(ln.Loglvl.VERBOSE, f'Adjusting learning rate [{lr}]')
-    for param_group in optim.param_groups:
-        param_group['lr'] = lr / BATCH
+        # Super init
+        super(CustomEngine, self).__init__(
+            net, optim, train, test, arguments.cuda, arguments.visdom,
+            batch_size=BATCH, batch_subdivisions=BATCH_SUBDIV, max_batch=MAX_BATCHES,
+            class_label_map=arguments.names, backup_folder=arguments.backup,
+            **kwargs
+            )
 
-    if batch < LR_STEPS[i]:
-        return LR_STEPS[i]
-
-def adjust_backup_rate(batch):
-    """ Adjust backup rate according to the batch number """
-    global BACKUP
-
-    if batch < BP_STEPS[0]:
-        return BP_STEPS[0]
-
-    idx = 0
-    for i in range(len(BP_STEPS)):
-        if batch >= BP_STEPS[i]:
-            idx = i
-        else:
-            break
-
-    ln.log(ln.Loglvl.VERBOSE, f'Adjusting backup rate [{BP_RATES[idx]}]')
-    BACKUP = BP_RATES[idx]
-
-    if batch < BP_STEPS[i]:
-        return BP_STEPS[i]
-
-def adjust_test_rate(batch):
-    """ Adjust test rate according to the batch number """
-    global TEST
-
-    if batch < TS_STEPS[0]:
-        return TS_STEPS[0]
-
-    idx = 0
-    for i in range(len(TS_STEPS)):
-        if batch >= TS_STEPS[i]:
-            idx = i
-        else:
-            break
-
-    ln.log(ln.Loglvl.VERBOSE, f'Adjusting test rate [{TS_RATES[idx]}]')
-    TEST = TS_RATES[idx]
-
-    if batch < TS_STEPS[i]:
-        return TS_STEPS[i]
-
-def train(net, optim, dataset):
-    """ Train the network for 1 epoch """
-    global LR_CUR_STEP
-    global BP_CUR_STEP
-    global TS_CUR_STEP
-
-    net.train()
-    kwargs = {'num_workers': 6, 'pin_memory': True} if args.cuda else {}
-    loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH//BATCH_SUBDIV, shuffle=True, drop_last=True, **kwargs)
-
-    # Adjust rates
-    batch = net.seen // BATCH
-    if LR_CUR_STEP is not None and batch >= LR_CUR_STEP:
-        LR_CUR_STEP = adjust_learning_rate(optim, batch)
-    if BP_CUR_STEP is not None and batch >= BP_CUR_STEP:
-        BP_CUR_STEP = adjust_backup_rate(batch)
-    if TS_CUR_STEP is not None and batch >= TS_CUR_STEP:
-        TS_CUR_STEP = adjust_test_rate(batch)
-
-    tot_minibatch = len(dataset) // (BATCH/BATCH_SUBDIV)
-    minibatch = 0
-    optim.zero_grad()
-    batch_loss = {'total': [], 'coord': [], 'conf': [], 'cls':[]}
-    for _, (data, target) in enumerate(loader):
-        # Forward & Backward
-        if args.cuda:
-            data = data.cuda()
-        data = Variable(data, requires_grad=True)
-        loss = net(data, target)
-        loss.backward()
-        batch_loss['total'].append(net.loss.loss_tot.data[0])
-        batch_loss['coord'].append(net.loss.loss_coord.data[0])
-        batch_loss['conf'].append(net.loss.loss_conf.data[0])
-        if net.loss.loss_cls is not None:
-            batch_loss['cls'].append(net.loss.loss_cls.data[0])
-
-        # Metadata
-        net.seen += data.size(0)
-        minibatch += 1
-
-        # Batch processed
-        if net.seen % BATCH == 0:
-            batch = net.seen // BATCH
-            optim.step()
-            optim.zero_grad()
-
-            # Visualisation
-            if len(batch_loss['cls']) > 0:
-                print(f"[TRAIN]    Batch:{batch} Loss:{round(mean(batch_loss['total']), 5)} (coord:{round(mean(batch_loss['coord']), 2)} conf:{round(mean(batch_loss['conf']), 2)} cls:{mean(batch_loss['cls'])})")
-            else:
-                print(f"[TRAIN]    Batch:{batch} Loss:{round(mean(batch_loss['total']), 5)} (coord:{round(mean(batch_loss['coord']), 2)} conf:{round(mean(batch_loss['conf']), 2)})")
-            batch_loss = {'total': [], 'coord': [], 'conf': [], 'cls':[]}
-            # TODO : Add visdom graphs
-
-            # Backup
-            if batch % BACKUP == 0:
-                net.save_weights(os.path.join(args.backup, f'weights_{batch}.pt'))
-
-            # Resize
-            if batch % 10 == 0:
-                net.change_input_dim()
-
-            # Adjust rates
-            if LR_CUR_STEP is not None and batch >= LR_CUR_STEP:
-                LR_CUR_STEP = adjust_learning_rate(optim, batch)
-            if BP_CUR_STEP is not None and batch >= BP_CUR_STEP:
-                BP_CUR_STEP = adjust_backup_rate(batch)
-            if TS_CUR_STEP is not None and batch >= TS_CUR_STEP:
-                TS_CUR_STEP = adjust_test_rate(batch)
-
-            # Maximum batches reached
-            if batch >= MAX_BATCHES:
-                return
-
-            # Not enough images left for new batch
-            if tot_minibatch - minibatch < BATCH//BATCH_SUBDIV:
-                return
-
-def test(net, dataset, final=False):
-    """ Test the network """
-    original_dim = net.input_dim
-    net.input_dim = NETWORK_SIZE
-    net.eval()
-    kwargs = {'num_workers': 4, 'pin_memory': False} if args.cuda else {}
-    loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH//BATCH_SUBDIV, collate_fn=ln.bbb_collate, **kwargs)
-
-    at = lnt.AnnoToTensor(net)
-    at.max = dataset.max_anno
-    tot_loss = []
-    anno, det = {}, {}
-    num_det = 0
-    for _, (data, target) in enumerate(loader):
-        # Forward
-        if args.cuda:
-            data = data.cuda()
-        data = Variable(data, volatile=True)
-        target_tensor = torch.stack([at(a) for a in target])
-        output, loss = net(data, target_tensor)
-
-        # Save output & target
-        for i in range(len(target)):
-            key = len(anno)
-            anno[key] = target[i]
-            det[key] = ln.bbox_to_brambox(output[i], net.input_dim, class_label_map=names)
-            num_det += len(det[key])
-
-        # Loss
-        tot_loss.append(loss.data[0]*len(target))
-
-    # Compute statistics
-    if num_det > 1:
-        ## TEMP
-        import cv2
+    def update(self):
+        """ Update rates according to batch """
+        # Test rate
         i = 0
-        while (len(det[i]) <= 0):
+        self.test_rate = TEST
+        while self.batch > TS_STEPS[i]:
+            self.test_rate = TS_RATES[i]
             i += 1
-        img = np.array((tf.ToPILImage()(dataset.img_tf(cv2.imread(dataset.id(dataset.keys[i]))))))
-        bbb.draw_box(img, anno[i], color=(0,0,255), inline=True)
-        detection = [det for det in det[i] if det.confidence >= .5]
-        bbb.draw_box(img, det[i], color=(0,255,0), inline=True)
-        if args.visdom:
-            viz.image(img, win='example-img')
+            if i >= len(TS_STEPS):
+                break
+
+        # Backup rate
+        i = 0
+        self.backup_rate = BACKUP
+        while self.batch > BP_STEPS[i]:
+            self.backup_rate = BP_RATES[i]
+            i += 1
+            if i >= len(BP_STEPS):
+                break
+
+        # Learning rate
+        lr = LEARNING_RATE
+        for i in range(len(LR_STEPS)):
+            if self.batch >= LR_STEPS[i]:
+                lr *= LR_SCALES[i]
+            else:
+                break
+        if lr/self.batch_size != self.learning_rate:
+            ln.log(ln.Loglvl.VERBOSE, f'Adjusting learning rate [{lr}]')
+            self.learning_rate = lr/self.batch_size
+
+        # Resize
+        if self.batch % RESIZE_RATE == 0:
+            self.network.change_input_dim()
+
+    def train(self):
+        tot_loss = []
+        coord_loss = []
+        conf_loss = []
+        cls_loss = []
+        
+        self.update()
+        self.optimizer.zero_grad()
+
+        loader = torch.utils.data.DataLoader(
+            self.trainset,
+            batch_size = self.mini_batch_size,
+            shuffle = True,
+            drop_last = True,
+            num_workers = WORKERS if self.cuda else 0,
+            pin_memory = PIN_MEM if self.cuda else False
+            )
+        for idx, (data, target) in enumerate(loader):
+            if self.cuda:
+                data = data.cuda()
+            data = Variable(data, requires_grad=True)
+            
+            loss = self.network(data, target)
+            loss.backward()
+            
+            tot_loss.append(self.network.loss.loss_tot.data[0])
+            coord_loss.append(self.network.loss.loss_coord.data[0])
+            conf_loss.append(self.network.loss.loss_conf.data[0])
+            if self.network.loss.loss_cls is not None:
+                cls_loss.append(self.network.loss.loss_cls.data[0])
+
+            if self.network.seen % self.batch_size == 0:
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+                tot = round(mean(tot_loss), 5)
+                coord = round(mean(coord_loss), 2)
+                conf = round(mean(conf_loss), 2)
+                self.visual(loss=tot, name='Total loss')
+                self.visual(loss=coord, name='Coordinate loss')
+                self.visual(loss=conf, name='Confidence loss')
+                if len(cls_loss) > 0:
+                    cls = round(mean(cls_loss), 2)
+                    self.visual(loss=cls, name='Class loss')
+                    self.log(f'{self.batch} Loss:{tot} (Coord:{coord} Conf:{conf} Cls:{cls})')
+                else:
+                    self.log(f'{self.batch} Loss:{tot} (Coord:{coord} Conf:{conf})')
+                tot_loss = []
+                coord_loss = []
+                conf_loss = []
+                cls_loss = []
+
+                if self.batch % self.backup_rate == 0:
+                    self.network.save_weights(os.path.join(self.backup_folder, f'weights_{self.batch}.pt'))
+
+                if self.sigint or self.batch >= self.max_batch or len(self.trainset) - (idx*self.mini_batch_size) < self.batch_size:
+                    return
+
+                self.update()
+
+    def test(self):
+        tot_loss = []
+        anno, det = {}, {}
+        num_det = 0
+        at = ln.data.AnnoToTensor(self.network)
+        at.max = self.testset.max_anno
+
+        saved_dim = self.network.input_dim
+        self.network.input_dim = NETWORK_SIZE
+
+        loader = torch.utils.data.DataLoader(
+            self.testset,
+            batch_size = self.mini_batch_size,
+            shuffle = False,
+            drop_last = False,
+            num_workers = WORKERS if self.cuda else 0,
+            pin_memory = PIN_MEM if self.cuda else False,
+            collate_fn = ln.data.bbb_collate,
+            )
+        for idx, (data, target) in enumerate(loader):
+            if self.cuda:
+                data = data.cuda()
+            data = Variable(data, volatile=True)
+            target_tensor = torch.stack([at(a) for a in target])
+
+            output, loss = self.network(data, target_tensor)
+
+            tot_loss.append(loss.data[0]*len(target))
+            for i in range(len(target)):
+                key = len(anno)
+                anno[key] = target[i]
+                det[key] = ln.data.bbox_to_brambox(output[i], self.network.input_dim, class_label_map=self.class_label_map)
+                num_det += len(det[key])
+
+            if self.sigint:
+                return
+
+        if num_det > 1:
+            pr = bbb.pr(det, anno, class_label_map=self.class_label_map)
+            m_ap = bbb.mean_ap(pr)
+            loss = round(sum(tot_loss)/len(anno), 5)
+
+            self.log(f'Loss:{loss} mAP:{round(m_ap*100, 2)}%')
+            self.visual(pr=pr)
+            self.visual(loss=loss, name='Total loss')
         else:
-            cv2.imwrite('.sandbox/image.png', img)
-        ##TEMP
-
-        pr = bbb.pr(det, anno)
-        m_ap = bbb.mean_ap(pr)
-
-        if args.visdom:
-            x = [val[1] for key, val in pr.items()]
-            y = [val[0] for key, val in pr.items()]
-            legend = [f'{key}: {round(bbb.ap(*val)*100, 2)}' for key, val in pr.items()]
-            viz.line(X=np.array(x).transpose(), Y=np.array(y).transpose(),
-                    win='testset',
-                    opts=dict(
-                        title='PR-curve testset',
-                        xlabel='Recall',
-                        ylabel='Precision',
-                        legend=legend,
-                        ))
-    else:
-        ln.log(ln.Loglvl.WARN, f'Not enough detections to perform advanced statistics [{num_det}]')
-
-    # Visualisation
-    if num_det > 1:
-        print(f'[TEST]     Loss:{round(sum(tot_loss)/len(anno), 5)} mAP:{round(m_ap*100, 2)}%')
-    else:
-        print(f'[TEST]     Loss:{sum(tot_loss)/len(anno)}')
-
-    # Reset network
-    net.input_dim = original_dim
+            ln.log(ln.Loglvl.WARN, f'Not enough detections to perform advanced statistics [{num_det}]')
+            self.log(f'Loss:{sum(tot_loss)/len(anno)}')
+        self.network.input_dim = saved_dim
 
 
 if __name__ == '__main__':
@@ -278,13 +224,13 @@ if __name__ == '__main__':
     parser.add_argument('weight', help='Path to weight file', default=None)
     parser.add_argument('train', help='File containing paths to training images')
     parser.add_argument('test', help='File containing paths to test images', nargs='?')
-    parser.add_argument('-c', '--cuda', action='store_true', help='Use cuda')
-    parser.add_argument('-v', '--visdom', action='store_true', help='Visualize training data with visdom')
     parser.add_argument('-b', '--backup', help='Backup folder', default='./backup')
     parser.add_argument('-n', '--names', help='Detection names file', default=None)
+    parser.add_argument('-c', '--cuda', action='store_true', help='Use cuda')
+    parser.add_argument('-v', '--visdom', action='store_true', help='Visualize training data with visdom')
     args = parser.parse_args()
 
-    # Parse Arguments
+    # Parse arguments
     if args.cuda:
         if not torch.cuda.is_available():
             ln.log(ln.Loglvl.ERROR, 'CUDA not available')
@@ -293,58 +239,21 @@ if __name__ == '__main__':
             ln.log(ln.Loglvl.DEBUG, 'CUDA enabled')
 
     if args.visdom:
-        import visdom
-        viz = visdom.Visdom(port=8080)
-        ln.log(ln.Loglvl.DEBUG, 'Visdom enabled')
+        args.visdom = VISDOM
+    else:
+        args.visdom = None
 
     if not os.path.isdir(args.backup):
         if not os.path.exists(args.backup):
             ln.log(ln.Loglvl.WARN, 'Backup folder does not exist, creating...')
             os.makedirs(args.backup)
         else:
-            ln.log(ln.Loglvl.ERROR, 'Backup path is not a folder')
-            sys.exit(1)
+            ln.log(ln.Loglvl.ERROR, 'Backup path is not a folder', ValueError)
 
     if args.names is not None:
         with open(args.names, 'r') as f:
-            names = f.read().splitlines()
-    else:
-        names = None;
+            args.names = f.read().splitlines()
 
-    # Create network
-    ln.log(ln.Loglvl.DEBUG, 'Creating network')
-    network, optimizer = create_network()
-
-    # Set rates
-    batch = network.seen//BATCH
-    LR_CUR_STEP = adjust_learning_rate(optimizer, batch)
-    BP_CUR_STEP = adjust_backup_rate(batch)
-    TS_CUR_STEP = adjust_test_rate(batch)
-    print()
-
-    # Create Datasets
-    ln.log(ln.Loglvl.DEBUG, 'Creating datasets')
-    trainset = ln.DarknetData(args.train, network)
-    if args.test is not None:
-        testset = ln.DarknetData(args.test, network, train=False, augment=False, class_label_map=names)
-    print()
-
-    # Main loop
-    train_batch = network.seen//BATCH - (network.seen//BATCH % TEST)
-    while True:
-        ln.log(ln.Loglvl.DEBUG, 'Starting train epoch')
-        train(network, optimizer, trainset)
-        batch = network.seen//BATCH
-
-        if args.test is not None and batch - train_batch >= TEST:
-            ln.log(ln.Loglvl.DEBUG, 'Starting test')
-            train_batch += TEST
-            test(network, testset)
-            print()
-        
-        if MAX_BATCHES is not None and batch >= MAX_BATCHES:
-            network.save_weights(os.path.join(args.backup, 'weights_final.pt'))
-            ln.log(ln.Loglvl.VERBOSE, f'Reached Maximum batch size [{batch}/{MAX_BATCHES}]')
-            if args.test is not None:
-                test(network, testset, True)
-            break
+    # Train
+    eng = CustomEngine(args)
+    eng()
