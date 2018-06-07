91d67df4d2e7afdc2a4128ec963731e716f14917
0phoff <tanguy.ophoff@kuleuven.be>
2018-06-07 16:53:04 +0200

    Setup.py allows for pillow-simd

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 import setuptools as setup
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+def get_dist(pkgname):
+    try:
+        return get_distribution(pkgname)
+    except DistributionNotFound:
+        return None
 
 def find_packages():
     return ['lightnet'] + ['lightnet.'+p for p in setup.find_packages('lightnet')]
@@ -11,22 +19,28 @@ def get_version():
         f.write('#   Lightnet version: Automatically generated version file\n')
         f.write('#\n\n')
         f.write(f'__version__ = "{version}"\n')
-    
+
     return version
 
-setup.setup(name='lightnet',
-            version=get_version(),
-            author='EAVISE',
-            description='Building blocks for recreating darknet networks in pytorch',
-            long_description=open('README.md').read(),
-            packages=find_packages(),
-            test_suite='test',
-            install_requires=[
-                'numpy',
-                'Pillow',
-                'brambox',
-            ],
-            extras_require={
-                'visual': ['visdom']
-            },
+
+requirements = [
+    'numpy',
+    'torchvision',
+    'brambox',
+]
+pillow_req = 'pillow-simd' if get_dist('pillow-simd') is not None else 'pillow'
+requirements.append(pillow_req)
+
+setup.setup(
+    name='lightnet',
+    version=get_version(),
+    author='EAVISE',
+    description='Building blocks for recreating darknet networks in pytorch',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    test_suite='test',
+    install_requires=requirements,
+    extras_require={
+        'visual': ['visdom']
+    },
 )
