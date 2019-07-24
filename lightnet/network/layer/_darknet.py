cb8e9403c977cf87ff45f74624dd8e16ac902834
0phoff <tanguy.ophoff@kuleuven.be>
2019-07-24 16:03:45 +0200

    Cleaned up layers
    Removed .data accessors, as nobody uses pytorch 0.3 anymore

--- lightnet/network/layer/_darknet.py
+++ lightnet/network/layer/_darknet.py
@@ -144,37 +144,25 @@ class Reorg(nn.Module):
     """
     def __init__(self, stride=2):
         super().__init__()
+        self.stride = stride
         if not isinstance(stride, int):
             raise TypeError(f'stride is not an int [{type(stride)}]')
-        self.stride = stride
-        self.darknet = True
 
     def extra_repr(self):
-        darknet_mode_str = ', darknet_compatible' if self.darknet else ''
-        return f'stride={self.stride}{darknet_mode_str}'
+        return f'stride={self.stride}'
 
     def forward(self, x):
-        assert(x.data.dim() == 4)
-        B = x.data.size(0)
-        C = x.data.size(1)
-        H = x.data.size(2)
-        W = x.data.size(3)
+        assert(x.dim() == 4)
+        B, C, H, W = x.size()
 
         if H % self.stride != 0:
             raise ValueError(f'Dimension mismatch: {H} is not divisible by {self.stride}')
         if W % self.stride != 0:
             raise ValueError(f'Dimension mismatch: {W} is not divisible by {self.stride}')
 
-        # darknet compatible version from: https://github.com/thtrieu/darkflow/issues/173#issuecomment-296048648
-        if self.darknet:
-            x = x.view(B, C//(self.stride**2), H, self.stride, W, self.stride).contiguous()
-            x = x.permute(0, 3, 5, 1, 2, 4).contiguous()
-            x = x.view(B, -1, H//self.stride, W//self.stride)
-        else:
-            ws, hs = self.stride, self.stride
-            x = x.view(B, C, H//hs, hs, W//ws, ws).transpose(3, 4).contiguous()
-            x = x.view(B, C, H//hs*W//ws, hs*ws).transpose(2, 3).contiguous()
-            x = x.view(B, C, hs*ws, H//hs, W//ws).transpose(1, 2).contiguous()
-            x = x.view(B, hs*ws*C, H//hs, W//ws)
+        # from: https://github.com/thtrieu/darkflow/issues/173#issuecomment-296048648
+        x = x.view(B, C//(self.stride**2), H, self.stride, W, self.stride).contiguous()
+        x = x.permute(0, 3, 5, 1, 2, 4).contiguous()
+        x = x.view(B, -1, H//self.stride, W//self.stride)
 
         return x
