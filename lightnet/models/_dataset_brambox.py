ee85aa7b697b8c5fae3eeca6425c85fe9790c9cb
0phoff <tanguy.ophoff@kuleuven.be>
2020-06-24 10:44:16 +0200

    Updated BramboxDataset to work with new compose pipelines

--- lightnet/models/_dataset_brambox.py
+++ lightnet/models/_dataset_brambox.py
@@ -27,21 +27,26 @@ class BramboxDataset(lnd.Dataset):
         input_dimension (tuple): (width,height) tuple with default dimensions of the network
         class_label_map (list): List of class_labels
         identify (function, optional): Lambda/function to get image based of annotation filename or image id; Default **replace/add .png extension to filename/id**
-        img_transform (torchvision.transforms.Compose): Transforms to perform on the images
-        anno_transform (torchvision.transforms.Compose): Transforms to perform on the annotations
+        transform (torchvision.transforms.Compose): Transformation pipeline
+        anno_transform (torchvision.transforms.Compose): Annotation transformation pipeline
+
+    Note:
+        If you only pass a ``transform`` pipeline, it will be called with both your image and annotations as a tuple.
+        However, if you pass both a ``transform`` and ``anno_transform`` pipeline, it will first run your image through the ``transform`` pipeline,
+        and then your annotations through ``anno_transform`` (legacy behaviour).
 
     Note:
         This dataset opens images with the Pillow library
     """
-    def __init__(self, annotations, input_dimension, class_label_map=None, identify=None, img_transform=None, anno_transform=None):
+    def __init__(self, annotations, input_dimension, class_label_map=None, identify=None, transform=None, anno_transform=None):
         if bb is None:
             raise ImportError('Brambox needs to be installed to use this dataset')
         super().__init__(input_dimension)
 
         self.annos = annotations
         self.keys = self.annos.image.cat.categories
-        self.img_tf = img_transform
-        self.anno_tf = anno_transform
+        self.transform = transform
+        self.anno_transform = anno_transform
 
         if callable(identify):
             self.id = identify
@@ -75,9 +80,11 @@ class BramboxDataset(lnd.Dataset):
         anno = bb.util.select_images(self.annos, [self.keys[index]])
 
         # Transform
-        if self.img_tf is not None:
-            img = self.img_tf(img)
-        if self.anno_tf is not None:
-            anno = self.anno_tf(anno)
+        if self.transform is not None and self.anno_transform is None:
+            (img, anno) = self.transform((img, anno))
+        elif self.transform is not None:
+            img = self.transform(img)
+            if self.anno_transform is not None:
+                anno = self.anno_transform(anno)
 
         return img, anno
